1b34799e616aacec90efb742011676e91bb17edb
faiqaj <94169064+faiqaj@users.noreply.github.com>
2021-12-02 13:27:14 +0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimore  <- subset(NEI, fips=="24510")
+baltimoretype <- aggregate(Emissions ~ year + type, baltimore, sum)
+
+library(ggplot2)
+g <- g + geom_line(lwd = 1.5) + xlab("Year") + ylab("Total PM2.5 Emissions (Tons)") + ggtitle('Total PM2.5 Emissions in Baltimore City, Maryland by Year & Type')
+print(g)
+
+dev.off()

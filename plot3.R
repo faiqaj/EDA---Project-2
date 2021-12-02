93f256595b5157b1760a04e62ac2d5655a45ef11
faiqaj <94169064+faiqaj@users.noreply.github.com>
2021-12-02 13:41:35 +0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,6 +5,7 @@ baltimore  <- subset(NEI, fips=="24510")
 baltimoretype <- aggregate(Emissions ~ year + type, baltimore, sum)
 
 library(ggplot2)
+png("plot3.png",width=640,height=480)
 g <- g + geom_line(lwd = 1.5) + xlab("Year") + ylab("Total PM2.5 Emissions (Tons)") + ggtitle('Total PM2.5 Emissions in Baltimore City, Maryland by Year & Type')
 print(g)
 
